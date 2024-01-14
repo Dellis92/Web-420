015ab1e31a43011af309ee9551f061451307993c
DeVonte <devonte.ellis@gmail.com>
2024-01-14 13:59:48 -0600

    Week 1 Folder

---
+++ app.js
@@ -0,0 +1,10 @@
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'WEB 420 RESTful APIs',
+            version: '1.0.0',
+        },
+    },
+    apis: ['./routes/*.js'],
+};
